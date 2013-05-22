88429d73be9140a6db6bbc1817657b303d7e7ecc
Mick <mick.odoherty@amodtech.com>
2013-05-22 19:37:09 +0100

    Interim - join, leave and get text all using asynchtasks

--- MeshDisplayClient/src/com/amodtech/meshdisplayclient/LiveMeshEventDisplayActivity.java
+++ MeshDisplayClient/src/com/amodtech/meshdisplayclient/LiveMeshEventDisplayActivity.java
@@ -1,20 +1,33 @@
 package com.amodtech.meshdisplayclient;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LiveMeshEventDisplayActivity extends Activity {
 	/*
@@ -39,7 +52,10 @@ public class LiveMeshEventDisplayActivity extends Activity {
         final Button backButton = (Button) findViewById(R.id.liveMeshDisplayBackButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            	finish();
+            	//Create an AsynchTask to message the server to leave the event
+            	//When the server responds the activity will finish
+            	LeaveEventTask leaveEventTask = new LeaveEventTask();
+            	leaveEventTask.execute();
             }
         });
 		
@@ -69,7 +85,7 @@ public class LiveMeshEventDisplayActivity extends Activity {
 		pollTask.execute();
 	}
 	
-	private class GetTextFromServerTask extends AsyncTask<String, Void, responseInfo> {
+	private class GetTextFromServerTask extends AsyncTask<String, ResponseInfo, Void> {
 		/*
 		 * Private AsynchTask to poll the server to get the text to display for this device
 		 */
@@ -78,46 +94,145 @@ public class LiveMeshEventDisplayActivity extends Activity {
     	MeshDisplayClientEngine meshDisplayEngine = appObject.getAppMeshDisplayEngine();
     	
         @Override
-        protected responseInfo doInBackground(String args[]) {
+        protected Void doInBackground(String... args) {
+            //Poll the server and send the POST request to the HTTP server and check that an OK is received
+        	
+        	//Continually pole the server until the task is cancelled
+        	while(true) {
+	        	InputStream is = null;
+	        	int response = 0;
+	            try {
+	                URL url = new URL(meshDisplayEngine.getServerBaseURL() + "/text_for_client/event_id" 
+	                				+ meshDisplayEngine.eventID + "/client_id/" + meshDisplayEngine.clintID);
+	                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	                conn.setReadTimeout(10000 /* milliseconds */);
+	                conn.setConnectTimeout(15000 /* milliseconds */);
+	                conn.setRequestMethod("GET");
+	                conn.setDoInput(true);
+	                
+	                // Starts the query
+	                conn.connect();
+	                
+	                //Check the response code and decode the text sent by the server
+	                response = conn.getResponseCode();
+	                is = conn.getInputStream();
+	                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	                String line = "";
+	                StringBuffer receivedMessage = new StringBuffer();
+	                while ((line = reader.readLine()) != null) {
+	                	receivedMessage = receivedMessage.append(line);
+	                }
+	                reader.close();
+	                JSONObject jsonResponseObject = new JSONObject(receivedMessage.toString());
+	                String textToDisplay = jsonResponseObject.getString("client_text");
+	                publishProgress(new ResponseInfo(response, textToDisplay));
+	            } catch (IOException e) {
+					//Some IO problem occurred - dump stack and inform caller
+	            	Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask", "exception posting join event request - response code: " + response);
+					e.printStackTrace();
+				} catch (JSONException e) {
+					//An Error occurred decoding the JSON
+					Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask", "exception parsing JSON response");
+					e.printStackTrace();
+				} finally {
+		            // Makes sure that the InputStream is closed after the app is
+		            // finished using it.
+	                if (is != null) {
+	                	try {
+	                		is.close();
+	                	} catch (IOException e) {
+	        				//Some IO problem occurred while closing is - just to a stack dump in this case
+	                		Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask", "exception closing is file");
+	        				e.printStackTrace();
+	                	}
+	                } 
+	            }
+	            
+	            //Sleep until the next poll
+	            try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					//Interrupted while sleeping - simply log this
+					Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask", "interupted while sleeping");
+				}
+        	}
+        }
+       
+        
+        @Override
+        protected void onProgressUpdate(ResponseInfo... responseInfo) {
+        	//The next text sent from the server will be reported in the respnse Info
+        	
+            //Check the result and update the text to display if the response code was OK
+        	if (responseInfo[0].responseCode == 200) {
+        		//Update the text to display
+        		meshDisplayEngine.textToDisplay = responseInfo[0].textToDisplay;
+        	} else {
+        		//Log an issue
+        		Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask","-> onPostExecute: unexpecetd resposne code: " + responseInfo[0].responseCode);
+        	}
+        }
+       
+	}
+	
+	private class ResponseInfo {
+		//Simple class to contain response codes
+		public int responseCode;
+		public String textToDisplay;
+		
+		public ResponseInfo(int resCode, String dispText) {
+			this.responseCode = resCode;
+			this.textToDisplay = dispText;
+			
+		}
+	}
+	
+	private class LeaveEventTask extends AsyncTask<String, Void, Integer> {
+		/*
+		 * Private AsynchTask to message server to join an event
+		 */
+        @Override
+        protected Integer doInBackground(String... args) {
+        	
+           	//Get the meshDisplayEngine object
+        	MeshDisplayApplictaion appObject = (MeshDisplayApplictaion)LiveMeshEventDisplayActivity.this.getApplicationContext();
+        	MeshDisplayClientEngine meshDisplayEngine = appObject.getAppMeshDisplayEngine();
+              
             //Send the POST request to the HTTP server and check that an OK is received
         	InputStream is = null;
         	int response = 0;
             try {
-                URL url = new URL(meshDisplayEngine.getServerBaseURL() + "/text_for_client/event_id" 
-                				+ meshDisplayEngine.eventID + "/client_id/" + meshDisplayEngine.clintID);
+                URL url = new URL(meshDisplayEngine.getServerBaseURL() + "/event_client_delete");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setReadTimeout(10000 /* milliseconds */);
                 conn.setConnectTimeout(15000 /* milliseconds */);
-                conn.setRequestMethod("GET");
+                conn.setRequestMethod("POST");
                 conn.setDoInput(true);
+                conn.setDoOutput(true);
+                
+                //Add the POST parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("event_id", meshDisplayEngine.eventID));
+                params.add(new BasicNameValuePair("client_id", meshDisplayEngine.clintID));
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getQuery(params));
+                writer.close();
+                os.close();
                 
                 // Starts the query
                 conn.connect();
                 
-                //Check the response code and decode the text sent by the server
+                //For this case just check the response code - we are not interested in the response itself
                 response = conn.getResponseCode();
                 is = conn.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                String line = "";
-                StringBuffer receivedMessage = new StringBuffer();
-                while ((line = reader.readLine()) != null) {
-                	receivedMessage = receivedMessage.append(line);
-                }
-                reader.close();
-                JSONObject jsonResponseObject = new JSONObject(receivedMessage.toString());
-                String textToDisplay = jsonResponseObject.getString("client_text");
-                return new responseInfo(response, textToDisplay);
-                publishProgress(new responseInfo(response, textToDisplay));
+                
             } catch (IOException e) {
 				//Some IO problem occurred - dump stack and inform caller
-            	Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask", "exception posting join event request - response code: " + response);
+            	Log.d("LiveMeshEventDisplayActivity LeaveEventTask", "exception posting leave event request - response code: " + response);
 				e.printStackTrace();
-				return new responseInfo(0, "");
-			} catch (JSONException e) {
-				//An Error occurred decoding the JSON
-				Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask", "exception parsing JSON response");
-				e.printStackTrace();
-				return new responseInfo(0, "");
+				response = 0;;
 			} finally {
 	            // Makes sure that the InputStream is closed after the app is
 	            // finished using it.
@@ -126,37 +241,52 @@ public class LiveMeshEventDisplayActivity extends Activity {
                 		is.close();
                 	} catch (IOException e) {
         				//Some IO problem occurred while closing is - just to a stack dump in this case
-                		Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask", "exception closing is file");
+                		Log.d("LiveMeshEventDisplayActivity LeaveEventTask", "exception closing is file");
         				e.printStackTrace();
                 	}
                 } 
             }
+            
+            //Set the client and event id to null
+        	meshDisplayEngine.eventID = null;
+        	meshDisplayEngine.clintID = null;
+        	
+        	return response;
         }
         
         @Override
-        protected void onPostExecute(responseInfo responseInfo) {
-        	// onPostExecute displays the results of the AsyncTask.
+        protected void onPostExecute(Integer responseCode) {
+        	//onPostExecute simply checks the response code and then finishes the Activity
         	
-            //Check the result and update the text to display if the response code was OK
-        	if (responseInfo.responseCode == 200) {
-        		//Update the text to display
-        		meshDisplayEngine.textToDisplay = responseInfo.textToDisplay;
-        	} else {
-        		//Log an issue
-        		Log.d("LiveMeshEventDisplayActivity GetTextFromServerTask","-> onPostExecute: unexpecetd resposne code: " + responseInfo.responseCode);
+            //Check the result and move to new Activity if appropriate
+        	if (responseCode != 200 & responseCode != 201) {
+        		//Log the problem leaving the event
+        		Log.d("LiveMeshEventDisplayActivity LeaveEventTask", "Unexpected response code for " +
+        				"leave event request - response code: " + responseCode);
         	}
+        	
+        	//Leave the activity
+        	finish();
        }
-	}
-	
-	private class responseInfo {
-		//Simple class to contain response codes
-		public int responseCode;
-		public String textToDisplay;
-		
-		public responseInfo(int resCode, String dispText) {
-			this.responseCode = resCode;
-			this.textToDisplay = dispText;
-			
-		}
-	}
+        
+        private String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException {
+        	//utility method to help build the post method - see: http://stackoverflow.com/a/13486223/334402
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            for (NameValuePair pair : params)
+            {
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
+            }
+
+            return result.toString();
+        }
+    }
 }
--- MeshDisplayClient/src/com/amodtech/meshdisplayclient/MeshDisplayClientEngine.java
+++ MeshDisplayClient/src/com/amodtech/meshdisplayclient/MeshDisplayClientEngine.java
@@ -1,14 +1,9 @@
 package com.amodtech.meshdisplayclient;
 
-import android.app.Activity;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-
 public class MeshDisplayClientEngine {
 	/*
-	 * This class is the main engine (or Model in MVC terms) of the Mesh Display
-	 * Client.
+	 * This class represents the engine of the Mesh Display Client. It contains
+	 * the eventID and clientID of the event, as well as the base URLs.
 	 */
 	
 	//Attributes
@@ -18,63 +13,6 @@ public class MeshDisplayClientEngine {
 	private final String MAMP_BASE_URL = "http://10.0.2.2:8888/codeigniter-restserver-master/index.php/api/example";
 	private final String serverBaseUrl = MAMP_BASE_URL;
 	public String textToDisplay = "";
-
-	public void joinEvent(String eventToJoin, String clientIDForEvent, Handler responseHandler) {
-		//This method adds this client to a live event
-		
-		//Check the client and event ids are not null
-		if (eventToJoin == null || clientIDForEvent == null) {
-			return;
-		}
-		
-		//If the event id is not null then we are already in an event so leave it first
-		if (this.eventID != null) {
-			this.leaveEvent();
-		}
-		
-		//Set the event and client ID
-		this.eventID = eventToJoin;
-		this.clintID = clientIDForEvent;
-		
-		//Message the server with the client and event ID
-		
-		//Start the timer to poll the server
-		
-		//Respond to the calling activity with a notification that the event has been started
-		Message newMessage = new Message();
-		newMessage.arg1 = 1;
-		responseHandler.sendMessage(newMessage);
-		Log.d("msg sent", "xxx");
-	}
-	
-	public void leaveEvent() {
-		// This method removes the client from a live event
-		
-		//Check first that there is a current event
-		if (this.eventID == null) {
-			return;
-		}
-		
-		//Message the server to remove the client from the event
-		
-		//Reset the event if and display
-		this.eventID = null;
-		this.textToDisplay = "";
-		
-		//Respond to the calling activity with a notification that the client has left the event
-	}
-	
-	public void getTextFromServer() {
-	    //This method polls the server looking for text to display. It polls the server 
-		//and it is recognised this may not be the most efficient from a better y point of view
-		//but it is considered sufficient for this proof of concept.
-		
-		//Message the server
-		
-		//Message the display activity to display the text
-		
-		
-	}
 	
 	public String getServerBaseURL() {
 		//Getter for the base URL
