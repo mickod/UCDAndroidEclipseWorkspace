5867ad347d17f9d555a992f8574d719c7441246e
Mick <mick.odoherty@amodtech.com>
2013-05-30 21:08:49 +0100

    Add Controller to the reporitory

---
+++ MeshDisplayController/src/com/amodtech/meshdisplaycontroller/EntryScreenAvtivity.java
@@ -0,0 +1,56 @@
+package com.amodtech.meshdisplaycontroller;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class EntryScreenAvtivity extends Activity {
+
+	/*
+	 * This class is the main menu Activity for the applictaion
+	 */
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_entry_screen);
+		
+		//Display the Main Menu image
+        ImageView mainMenuImageView;
+        mainMenuImageView = (ImageView) findViewById(R.id.mainMenuPicture);
+        int imageResID = getResources().getIdentifier("controller_menu_image", "drawable",  getPackageName());
+        if (imageResID != 0) {
+        	mainMenuImageView.setImageResource(imageResID);
+        }
+        
+        //Set the enter manually button listener
+        final Button menuManualEnterButton = (Button) findViewById(R.id.enterEventButton);
+        menuManualEnterButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+        
+        //Set the about button listener
+        final Button menuAboutButton = (Button) findViewById(R.id.aboutButton);
+        menuAboutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AboutMeshDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+}
